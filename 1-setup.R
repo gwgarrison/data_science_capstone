0f8372be31a4a364f9f20bd34a281c53f701cd2d
Gary Garrison <garrisongw@gmail.com>
2015-03-19 06:12:40 -0600

    added milestone report

--- 1-setup.R
+++ 1-setup.R
@@ -3,7 +3,7 @@
 # setup
 library(tm);library(SnowballC);#library(qdap);library(qdapDictionaries);
 library(plyr);library(RColorBrewer);library(ggplot2); library(scales);library(wordcloud)
-library(RWeka)
+library(RWeka);library(slam)
 
 # get line count for input file
 #tmp <- readChar("en_US.blogs.txt", file.info("en_US.blogs.txt")$size)
@@ -12,14 +12,24 @@ library(RWeka)
 #system.time(doc <- readLines("en_US.blogs.txt",n = 100))
 
 # get only a sample of the file
-#system.time(doc <- sample.file("en_US.blogs.txt",.02))
-#write(doc,"sample.txt")
+#source("sample.file.R")
+# system.time(blogs <- sample.file("final/en_US/en_US.blogs.txt",.01))
+#system.time(news <- sample.file("final/en_US/en_US.news.txt",.01))
+# system.time(twitter <- sample.file("final/en_US/en_US.twitter.txt",.01))
+# write(blogs,"samples/blogs.txt")
+# write(news,"samples/news.txt")
+# write(twitter,"samples/twitter.txt")
 
-doc <- readLines("sample.txt")
+blogs <- readLines("samples/blogs.txt")
+news <- readLines("samples/news.txt")
+twitter <- readLines("samples/twitter.txt")
+
+# combine docs
+doc <- c(blogs,news,twitter)
 
 #system.time(crps <- Corpus(DirSource(), readerControl = list(language="en_US")))
 raw.crps <- Corpus(VectorSource(doc), readerControl = list(language="english"))
-crps <- Corpus(VectorSource(doc), readerControl = list(language="english"))
+crps <- raw.crps #Corpus(VectorSource(doc), readerControl = list(language="english"))
 
 crps <- tm_map(crps,removeNumbers)
 crps <- tm_map(crps,removePunctuation)
@@ -42,7 +52,10 @@ inspect(dtm[1:5, 1000:1005])
 # Explore the corpus.
 findFreqTerms(dtm, lowfreq=30)
 findAssocs(dtm, "data", corlimit=0.3)
-freq <- sort(colSums(as.matrix(dtm)), decreasing=TRUE)
+d <- removeSparseTerms(dtm,.6)
+#freq <- sort(colSums(as.matrix(dtm)), decreasing=TRUE)
+dtm.ru <- rollup(dtm, 2, na.rm=TRUE, FUN = sum)
+dtm.df <- as.data.frame(as.matrix(dtm.ru))
 wf   <- data.frame(word=names(freq), freq=freq)
 
 
