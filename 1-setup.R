98360c313020a6bc7ab4019e440f6efce204c195
Gary Garrison <garrisongw@gmail.com>
2015-03-20 05:55:25 -0600

    added profanity filter

--- 1-setup.R
+++ 1-setup.R
@@ -12,7 +12,7 @@ library(RWeka);library(slam)
 #system.time(doc <- readLines("en_US.blogs.txt",n = 100))
 
 # get only a sample of the file
-#source("sample.file.R")
+source("sample.file.R")
 # system.time(blogs <- sample.file("final/en_US/en_US.blogs.txt",.01))
 #system.time(news <- sample.file("final/en_US/en_US.news.txt",.01))
 # system.time(twitter <- sample.file("final/en_US/en_US.twitter.txt",.01))
@@ -31,9 +31,16 @@ doc <- c(blogs,news,twitter)
 raw.crps <- Corpus(VectorSource(doc), readerControl = list(language="english"))
 crps <- raw.crps #Corpus(VectorSource(doc), readerControl = list(language="english"))
 
+# get rid of the numbers and punctuation
 crps <- tm_map(crps,removeNumbers)
 crps <- tm_map(crps,removePunctuation)
-crps <- tm_map(crps,stripWhitespace)
+
+# remove profanity
+profanity.df <- read.table("profanity.txt")
+profanity <- as.character(profanity.df[,1])
+
+crps <- tm_map(crps,removeWords,profanity)
+#crps <- tm_map(crps,stripWhitespace)
 
 # function to get rid of non standard characters
 toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
@@ -52,8 +59,8 @@ inspect(dtm[1:5, 1000:1005])
 # Explore the corpus.
 findFreqTerms(dtm, lowfreq=30)
 findAssocs(dtm, "data", corlimit=0.3)
-d <- removeSparseTerms(dtm,.6)
-#freq <- sort(colSums(as.matrix(dtm)), decreasing=TRUE)
+d <- removeSparseTerms(dtm,.98)
+freq <- sort(colSums(as.matrix(dtm)), decreasing=TRUE)
 dtm.ru <- rollup(dtm, 2, na.rm=TRUE, FUN = sum)
 dtm.df <- as.data.frame(as.matrix(dtm.ru))
 wf   <- data.frame(word=names(freq), freq=freq)
