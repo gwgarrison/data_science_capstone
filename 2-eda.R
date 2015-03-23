20b2a5051fadf903a68b58e14a5576ed5e80828b
Gary Garrison <garrisongw@gmail.com>
2015-03-23 11:35:26 -0600

    added unigram counts

--- 2-eda.R
+++ 2-eda.R
@@ -18,18 +18,25 @@ BigramTokenizer <- function(x) RWeka::NGramTokenizer(x, RWeka::Weka_control(min 
 TrigramTokenizer <- function(x) RWeka::NGramTokenizer(x, RWeka::Weka_control(min = 3, max = 3))
 bdm <- TermDocumentMatrix(crps, control = list(tokenize = BigramTokenizer))
 tdm <- TermDocumentMatrix(crps, control = list(tokenize = TrigramTokenizer))
+udm <- TermDocumentMatrix(crps)
 dm <- removeSparseTerms(bdm, 0.9999)
 tm <- removeSparseTerms(tdm, 0.9999)
-dm <- as.matrix(dm)
+um <- removeSparseTerms(udm, 0.9999)
 tm <- as.matrix(tm)
+dm <- as.matrix(dm)
+um <- as.matrix(um)
 b.freq <- as.data.frame(rowSums(dm))
 t.freq <- as.data.frame(rowSums(tm))
+u.freq <- as.data.frame(rowSums(um))
 b.freq$bigram <- row.names(b.freq)
 t.freq$trigram <- row.names(t.freq)
+u.freq$unigram <- row.names(u.freq)
+uni.freq <- tbl_df(data.frame(u.freq[,2],u.freq[,1]))
 bi.freq <- tbl_df(data.frame(b.freq[,2],b.freq[,1]))
 tri.freq <- tbl_df(data.frame(t.freq[,2],t.freq[,1]))
 names(bi.freq) <- c("bigram","count")
 names(tri.freq) <- c("trigram","count")
+names(uni.freq) <- c("unigram","count")
 
 
 p <- ggplot(subset(wf, freq>2000), aes(word, freq))
@@ -39,7 +46,8 @@ p
 
 # word cloud
 set.seed(77)
-wordcloud(names(freq), freq, min.freq=1000, colors=brewer.pal(6, "Dark2"))
+system.time(wordcloud(names(freq), freq, min.freq=700, colors=brewer.pal(6, "Dark2")))
 
 # save current work
-system.time(save.image("work_20150321.rda"))
+system.time(save.image("work_20150323.rda"))
+load("work_20150323.rda")
