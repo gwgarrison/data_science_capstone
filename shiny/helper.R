07109192fb8ea6032aa4fda39274ac32f233dcbd
Gary Garrison <garrisongw@gmail.com>
2015-03-31 06:04:14 -0600

    added functionality for predicting 2 and larger strings

--- shiny/helper.R
+++ shiny/helper.R
@@ -1,7 +1,7 @@
-library(stringi)
-library(dplyr)
 
 predict_next_word <- function(words, number_of_preds = 3){
+library(stringi)
+library(dplyr)
   
   words <- stri_extract_all_words(words)
   words.size <- length(words[[1]])
@@ -12,6 +12,15 @@ predict_next_word <- function(words, number_of_preds = 3){
     
      bi <- filter(bi.freq,grepl(paste('^',words,' ',sep=''),bigram))
      bi <- arrange(bi,desc(count))[1:3,]
+     bi[,1]    
+  } else if (words.size > 1){
+    
+    # only take the last two words in the string
+    words <- paste(words[[1]][words.size-1],words[[1]][words.size])
+    tri <- filter(tri.freq,grepl(paste('^',words,' ',sep=''),trigram))
+    tri <- arrange(tri,desc(count))[1:3,]
+    tri[,1]    
+    
     
   }
   
