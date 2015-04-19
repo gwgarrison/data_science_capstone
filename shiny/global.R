faec088777b4987ceea2bd4927d169bafdce3169
Gary Garrison <garrisongw@gmail.com>
2015-04-18 20:58:56 -0600

    after backoff

--- shiny/global.R
+++ shiny/global.R
@@ -23,12 +23,26 @@ predict_next_word <- function(words, number_of_preds = 3){
     wf <- filter(tri.freq,grepl(paste('^',words,' ',sep=''),trigram))
     wf <- arrange(wf,desc(count))[1,]
     
+    # if no good prediction backoff to bigram
+    if (is.na(wf[,1])){
+       words <- words[[1]][words.size]
+       wf <- filter(bi.freq,grepl(paste('^',words,' ',sep=''),bigram))
+       wf <- arrange(wf,desc(count))[1,]
+    }
+    
   } else if (words.size >= 3) {
     
     # for 3 words and higher use the quad gram to predict the next word
     words <- paste(words[[1]][words.size-2],words[[1]][words.size-1],words[[1]][words.size])
     wf <- filter(quad.freq,grepl(paste('^',words,' ',sep=''),quadgram))
     wf <- arrange(wf,desc(count))[1,]
+    
+    # if no good prediction backoff to trigram
+    if ( is.na(wf[1])){
+       words <- paste(words[[1]][words.size-1],words[[1]][words.size])
+       wf <- filter(tri.freq,grepl(paste('^',words,' ',sep=''),trigram))
+       wf <- arrange(wf,desc(count))[1,]
+    }
   
   } #else if (words.size > 3 ){
     
@@ -45,15 +59,15 @@ wf <- wf[,1]
 names(wf) <- "gram"
 w <- gsub(words,'',wf$gram)
 w <- sub(' ','',w)
-if ( is.na(w[1]) ){
-  
-  w <- arrange(uni.freq,desc(count))[1,]
-  w <- as.character(w$unigram)
-  #print()
-} 
+
+# if ( is.na(w[1]) ){
+#   
+#   w <- arrange(uni.freq,desc(count))[1,]
+#   w <- as.character(w$unigram)
+#   #print()
+# } 
 #print(words,row.names= FALSE)
 w
-  
 }
 
 call_predict <- function(words){
@@ -61,9 +75,13 @@ call_predict <- function(words){
   w <- ''
   if (nchar(words) > 0) {
      w <- predict_next_word(words)
+     wl <- unlist(strsplit(w,''))
+     if ( is.na(w[1])){
+        w <- arrange(uni.freq,desc(count))[1,]
+        w <- as.character(w$unigram)
+     }
   }  else {
     w <- '<Please type some input to get a word prediction>'
   }
   w
-  
 }
